79959495448e47fe66d31a85b4949c1dc8fd5da5
SergeyBunin443 <113151370+SergeyBunin443@users.noreply.github.com>
2022-12-15 21:58:22 -0500

    Add files via upload

---
+++ final-starter-files/js/main.js
@@ -0,0 +1,35 @@
+console.log("JavaScript Portion of the Test");
+
+// Step 1: Write a multi-line comment detailing your name and student number
+/*
+Sergey Bunin
+200531423
+*/
+
+const myImmutableArray = ["Jared", "Francis", "Amrit", "Sarah", "Gurleen", "Johanna", "Ury"];
+
+
+// Step 2: Add the 3 people's names to the array
+myImmutableArray.push("Daniel","Jacob","Christian");
+
+// Step 4: Remove the first array item from the array
+myImmutableArray.shift();
+
+// Step 5 Remove the second array item from the array
+myImmutableArray.splice(1,1)
+
+// Step 6: Using array filter, iterate over the array items and remove all items beginning with the letter "J". Store the result in a new, block scoped, mutable variable
+const first = myImmutableArray.find(function (item) {
+    if (word[0] === J) {
+        myImmutableArray.splice(word);
+    } else {
+        return false;
+    }
+});
+console.log(first);
+
+// Step 7: Create an event listener that fires a callback function when the call to action button in the header is clicked. It should alert "Call me now at 555-555-55555";
+const button = document.querySelector('button');
+button.addEventListener("click", function () {
+    console.log("Call me now at 555-555-55555");
+    alert("Call me now at 555-555-55555");});
